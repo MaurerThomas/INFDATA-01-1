beb54df313c3fc51c97beda097a87679cfcbfbc6
ArmindoMaurits <a.r.maurits@gmail.com>
2016-03-01 09:45:10 +0100

    Init updated met conventies

--- src/main/java/Start/Init.java
+++ src/main/java/Start/Init.java
@@ -1,26 +1,32 @@
+package Start;
+
 import datastructure.DataSetLoader;
 import datastructure.User;
 
 import java.io.IOException;
 import java.util.TreeMap;
+import java.util.logging.*;
 
 /**
  * Created by armindo on 29-2-2016.
  */
 public class Init {
 
+    private Init(){}
+
     public static void main(String[] args) {
         DataSetLoader dsl = new DataSetLoader();
         TreeMap TM = new TreeMap<>();
+        Logger logger = Logger.getLogger("myLogger");
         try {
            TM = dsl.LoadDataSet();
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "Could not init: ", e);
         }
 
         //Get rating of two users on one movie
 
-        User bla = (User) TM.get(1);
-        float ratingkje = bla.getMovieRating(106);
+        User UserOne = (User) TM.get(1);
+        float UserOneRating = UserOne.getMovieRating(106);
     }
 }
